0ba38830fca17d783e817a94f218fa2a4647e3c0
myla48 <ruhamazerihun2@gmail.com>
2025-11-07 11:54:38 +0300

    Update Header.jsx

--- alx-react-app-new/Header.jsx
+++ alx-react-app-new/Header.jsx
@@ -1,14 +1,11 @@
+import React from 'react';
+
 function Header() {
   return (
-    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '20px' }}>
       <h1>My Favorite Cities</h1>
     </header>
   );
 }
 
 export default Header;
-
-
-<header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
-  <h1>My Favorite Cities</h1>
-</header>
