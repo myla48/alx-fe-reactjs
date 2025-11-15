010fc630547b0701acdb6d843e1810353a5066f5
myla48 <ruhamazerihun2@gmail.com>
2025-11-16 00:46:37 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,25 +1,20 @@
 import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
-  // State: list of recipes
   recipes: [],
 
-  // Action: add a new recipe
   addRecipe: (newRecipe) =>
     set((state) => ({
       recipes: [...state.recipes, newRecipe],
     })),
 
-  // Action: replace the entire recipe list
   setRecipes: (recipes) => set({ recipes }),
 
-  // (Optional for later tasks) delete a recipe by ID
   deleteRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
 
-  // (Optional for later tasks) update a recipe by ID
   updateRecipe: (id, updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
