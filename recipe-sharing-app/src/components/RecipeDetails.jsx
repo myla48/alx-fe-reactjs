62f641e13ed36e2656866b86a4344a4d7ce91c6c
myla48 <ruhamazerihun2@gmail.com>
2025-11-16 00:47:08 +0300

    Create RecipeDetails.jsx

---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,18 @@
+import useRecipeStore from '../recipeStore';
+
+const RecipeDetails = ({ recipeId }) => {
+  const recipe = useRecipeStore(state =>
+    state.recipes.find(r => r.id === recipeId)
+  );
+
+  if (!recipe) return <p>Recipe not found.</p>;
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+    </div>
+  );
+};
+
+export default RecipeDetails;
