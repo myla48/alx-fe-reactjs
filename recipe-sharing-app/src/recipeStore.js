ae65461a14aae298aec6fbf7e62087aebe6a74b3
myla48 <ruhamazerihun2@gmail.com>
2025-11-16 00:55:43 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -1,31 +1,18 @@
 import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
-  // State: list of recipes
   recipes: [],
 
-  // Search state
-  searchTerm: '',
-
-  // Action: add a new recipe
   addRecipe: (newRecipe) =>
     set((state) => ({
       recipes: [...state.recipes, newRecipe],
     })),
 
-  // Action: replace the entire recipe list
-  setRecipes: (recipes) => set({ recipes }),
-
-  // Action: update search term
-  setSearchTerm: (term) => set({ searchTerm: term }),
-
-  // Action: delete a recipe by ID
   deleteRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
 
-  // Action: update a recipe by ID
   updateRecipe: (id, updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
