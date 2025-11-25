527824416a03392797d0aaea0cc6d1fe2251f892
myla48 <ruhamazerihun2@gmail.com>
2025-11-25 10:48:40 +0300

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,10 +1,10 @@
 // src/App.jsx
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello, Tailwind + React!
-      </h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <UserProfile />
     </div>
   );
 }
