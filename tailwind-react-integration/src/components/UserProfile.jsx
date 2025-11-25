a13d98c89963dadeead48bc0c5633a3f59f6ae4d
myla48 <ruhamazerihun2@gmail.com>
2025-11-25 10:51:47 +0300

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 // src/components/UserProfile.jsx
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
         className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto"
       />
-      <h1 className="text-lg sm:text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-sm sm:text-base">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
