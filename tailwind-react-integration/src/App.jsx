d48f0a0b4bed844f9ca9fe1d462b643c1b6c2cd2
myla48 <ruhamazerihun2@gmail.com>
2025-11-25 10:44:58 +0300

    Create App.jsx

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello, Tailwind + React!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
